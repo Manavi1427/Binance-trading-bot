be7fc234eeda31d8c47a900f3f4adbf37ec7c0d9
Manavi1427 <manavisarkar136@gmail.com>
2026-06-13 23:54:31 +0530

    client.py

---
+++ bot/client.py
@@ -0,0 +1,32 @@
+#finance client 
+#loading api credentials - initialise binance client - return client object
+
+import os 
+from dotenv import load_dotenv
+from binance.client import Client
+
+TESTNET_URL = "https://testnet.binancefuture.com"
+
+def get_client():
+    #create and return a binance futures client
+
+    load_dotenv()
+
+    api_key = os.getenv("BINANCE_API_KEY")
+    api_secret = os.getenv("BINANCE_API_SECRET")
+
+    if not api_key or not api_secret:
+        raise ValueError(
+            "API credentials not found. "
+            "Please set BINANCE_API_KEY and BINANCE_SECRET_KEY in .env"
+        )
+    
+    client = Client(api_key,api_secret)
+    #redirect future request to testnet
+
+    client.FUTURES_URL = f"{TESTNET_URL}/fapi"
+    return client 
+
+
+    
+
