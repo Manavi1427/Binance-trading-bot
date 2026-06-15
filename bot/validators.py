c131bee82701b2ee4357c6f9a7b54782a3f77b45
Manavi1427 <manavisarkar136@gmail.com>
2026-06-15 23:56:12 +0530

    symbol validator

--- bot/validators.py
+++ bot/validators.py
@@ -52,6 +52,64 @@ def valid_side(side):
         )
     return side 
 
+def validate_order_type(order_type):
+    #validate market/limit
+    order_type = order_type.upper()
+
+    if order_type not in VALID_ORDER_TYPES:
+        raise ValidationError(
+            f"Invalid order type {order_type}, Choose from {VALID_ORDER_TYPES}"
+        )
+    
+    return order_type
+
+
+def validate_symbol(self, symbol):
+    #validate trading symbol
+
+    symbol = symbol.upper()
+    if symbol not in self.symbol_rules:
+        raise ValidationError(
+            f"Invalid symbol {symbol}"
+        )
+    return symbol
+
+def validate_quantity(self, quanity,symbol):
+    #Validate quanitity using binance lot rules
+
+    try:
+        quanity= Decimal(str(quanity))
+    except InvalidOperation:
+        raise ValidationError(
+            "Quanity must be numeric"
+        )
+    
+    rules = self.symbol_rules["symbol"]
+
+    if quanity < rules["min_qty"]:
+        raise ValidationError(
+            f"Quantity must be gretater than {rules["min_qty"]}"
+        )
+    
+    if quanity > rules["max_qty"]:
+        raise ValidationError(
+            f"Quantity must be greater than {rules["max_qty"]}"
+        )
+    
+    step = rules["step_size"]
+
+    remainder = quanity % step
+
+    if remainder != 0:
+        raise ValidationError(
+            f"Quantity must follow step size {step}"
+        )
+    
+    return quanity
+
+
+
+
 
 
 
