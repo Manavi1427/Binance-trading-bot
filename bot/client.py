1a6157c0456d380c8d23a19e2e52aa1cf3751e13
Manavi1427 <manavisarkar136@gmail.com>
2026-06-20 23:27:00 +0530

    api updated

--- bot/client.py
+++ bot/client.py
@@ -7,10 +7,17 @@ from binance.client import Client
 
 TESTNET_URL = "https://testnet.binancefuture.com"
 
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
+
+api_key = os.getenv("BINANCE_API_KEY")
+api_secret = os.getenv("BINANCE_SECRET_KEY")
+
 def get_client():
     #create and return a binance futures client
 
-    load_dotenv()
 
     api_key = os.getenv("BINANCE_API_KEY")
     api_secret = os.getenv("BINANCE_SECRET_KEY")
@@ -27,7 +34,10 @@ def get_client():
     client.FUTURES_URL = f"{TESTNET_URL}/fapi"
     return client 
 
-
+print("API:", api_key[:10])
+print("SECRET:", api_secret[:10])
+print(len(api_key))
+print(len(api_secret))
 
 
     
--- tests/test_client.py
+++ tests/test_client.py
@@ -2,5 +2,13 @@ from bot.client import get_client
 
 client = get_client()
 
-print("connected succesfully")
-print(client.futures_account_balance)
+print("Ping:", client.futures_ping())
+
+print("Server Time:")
+print(client.futures_time())
+
+print("Account:")
+print(client.futures_account())
+
+print("Balance:")
+print(client.futures_account_balance())
